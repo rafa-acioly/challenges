2364ac056a4fb7f5f3e8790e63e6181b32500af8
Rafael Acioly <aciolyr@gmail.com>
2020-09-11 07:19:38 -0300

    create dogshero own mod

--- doghero/go.mod
+++ doghero/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/google/uuid v1.1.2
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/stretchr/testify v1.6.1
+	gopkg.in/DATA-DOG/go-sqlmock.v1 v1.3.0
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gorm.io/gorm v1.20.0
 )
--- doghero/repository.go
+++ doghero/repository.go
@@ -2,35 +2,61 @@ package doghero
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
-type Repository struct {
+type Repository interface {
+	Index(filter WalkingFilter, page int) []DogWalking
+	Create(walking DogWalking) bool
+	StartWalk(walkingUUID string) (time.Time, error)
+	FinishWalk(walkingUUID string) (time.Time, error)
+}
+
+type repository struct {
 	database *sql.DB
 }
 
 // NewRepository retrieve a repository instance connection
 // to the walking database
 func NewRepository(db *sql.DB) Repository {
-	return Repository{database: db}
+	return &repository{database: db}
 }
 
 // Index retrieve a list of walking
-func (r *Repository) Index(filter WalkingFilter, page int) []DogWalking {
+func (r repository) Index(filter WalkingFilter, page int) []DogWalking {
 	return []DogWalking{}
 }
 
 // Create insert a new walking record on the database
-func (r *Repository) Create(walking DogWalking) bool {
+func (r repository) Create(walking DogWalking) bool {
 	return true
 }
 
-// StartWalk set the "StartAt" key to the current time
-func (r *Repository) StartWalk(walkingUUID string) (time.Time, error) {
-	return time.Time{}, nil
+// StartWalk set the "StartAt" key to the current time if it is not defined yet
+func (r *repository) StartWalk(walkingUUID string) (time.Time, error) {
+	started := time.Now()
+
+	query := "UPDATE table SET start_at = $1 WHERE uuid = $2 AND start_at = null"
+	stmt, err := r.database.Prepare(query)
+	if err != nil {
+		return time.Time{}, err
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(started, walkingUUID)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	if row, _ := result.RowsAffected(); row == 0 {
+		return time.Time{}, errors.New("walking already started or does not exist")
+	}
+
+	return started, nil
 }
 
 // FinishWalk set the "EndAt" key to the current time
-func (r *Repository) FinishWalk(walkingUUID string) (time.Time, error) {
+func (r *repository) FinishWalk(walkingUUID string) (time.Time, error) {
 	return time.Time{}, nil
 }

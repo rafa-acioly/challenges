2c3fe842629090adf398b678a6c4b782a6ad29b8
Rafael Acioly <aciolyr@gmail.com>
2020-09-13 19:09:21 -0300

    adds list endpoint

--- doghero/api/api.go
+++ doghero/api/api.go
@@ -21,14 +21,20 @@ func NewDogWalkResource(db *sql.DB) Resource {
 
 // Routes attach the http verbs and handlers to the resource
 func (rsc *Resource) Routes(router chi.Router) {
-	router.Get("/", rsc.Get)
+	router.Get("/{id}/", rsc.Get)
+	router.Get("/", rsc.List)
 	router.Post("/", rsc.Post)
 	router.Put("/{id}/", rsc.Put)
 }
 
-// Get represents the HTTP Get verb
+// Get represents the HTTP Get verb to get a single walk resource
 func (rsc *Resource) Get(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Get resource")
+}
+
+// List represents the HTTP Get verb to list the walk resources
+func (rsc *Resource) List(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "List resource")
 }
 
 // Post represents the HTTP Post verb
